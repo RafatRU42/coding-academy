d7f7d219a2880b2d3fa968aecf17f5f2446eb349
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-03-29 16:06:57 +0600

    authentication routes are added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import Main from '../Layout/Main'
 import ErrorPage from '../Pages/ErrorPage'
 import Home from '../Pages/Home/Home'
+import Signup from '../Pages/SignUp/SignUP'
 
 export const routes = createBrowserRouter([
     {
         path:'/', element:<Main></Main>, children:[
             {path:'/', element:<Home></Home>},
-            {path:'/error', element:<ErrorPage></ErrorPage>},
+            {path:'/signup', element:<Signup></Signup>},
         ]
     }
 ]) 
